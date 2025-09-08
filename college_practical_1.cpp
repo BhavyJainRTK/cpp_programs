8b98d23f4d1420ed19a04abe689c59d4781a1225
BhavyJainRTK <bhavy2142006@gmail.com>
2025-09-08 14:15:02 +0530

    add new program

---
+++ college_practical_1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+class rect
+{
+    int length;
+    int breadth;
+
+public:
+    void get_data()
+    {
+        cout << "Enter the length > ";
+        cin >> length;
+
+        cout << "Enter the breadth > ";
+        cin >> breadth;
+    }
+
+    void show_data()
+    {
+        cout << "length > " << length << endl;
+        cout << "breadth > " << breadth << endl;
+    }
+};
+
+int main()
+{
+    rect obj1;
+
+    obj1.get_data();
+    obj1.show_data();
+
+    rect obj2;
+
+    obj2.get_data();
+    obj2.show_data();
+}
